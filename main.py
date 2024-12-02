1dd0ab48f07c2b11a2ab652c85e38c89768cb396
nikkoz0 <160867433+nikkoz0@users.noreply.github.com>
2024-12-02 19:21:54 +0500

    homework_3

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import sys
 from random import randint
 from PyQt6.QtWidgets import QMainWindow, QApplication
 from PyQt6.QtGui import QPainter, QColor
-from PyQt6 import uic
+from homework_3 import Ui_MainWindow
 
 
-class Homework(QMainWindow):
+class Homework(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
         self.flag = False
-        uic.loadUi('homework_2.ui', self)
         self.pushButton.clicked.connect(self.push)
 
     def push(self):
@@ -20,10 +20,11 @@ class Homework(QMainWindow):
         if self.flag:
             painter = QPainter()
             painter.begin(self)
-            painter.setBrush(QColor(255, 255, 0))
+            painter.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
             r = randint(1, 200)
             painter.drawEllipse(200, 60, r, r)
             r_1 = randint(1, 200)
+            painter.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
             painter.drawEllipse(400, 60, r_1, r_1)
             painter.end()
         self.flag = False
